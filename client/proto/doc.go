d890d9d2581dd9b3e0849a9185575e62b9c3bd03
Steven L <imgroxx@gmail.com>
2024-06-29 21:44:42 -0400

    some notes

--- client/proto/doc.go
+++ client/proto/doc.go
@@ -1,6 +1,9 @@
 /*
 Package proto is a separate module that implements a GRPC Cadence client, for sending requests.
 
+Or it will be when it's complete.
+For now, it's just reserving space, for when we fully remove Proto from the main module's dependencies.
+
 Keeping this a separate package ensures both we and our users are not bound to a specific GRPC/Protobuf code generator,
 and that we do not restrict their versions by our choices.
 */
--- client/thrift/doc.go
+++ client/thrift/doc.go
@@ -1,6 +1,9 @@
 /*
 Package thrift is a separate module that implements a Thrift Cadence client, for sending requests.
 
+Or it will be when it's complete.
+For now, it's just reserving space, for when we fully remove Thrift from the main module's dependencies.
+
 Keeping this a separate package ensures both we and our users are not bound to a specific Thrift code version,
 and that we do not restrict their versions by our choices.
 */
